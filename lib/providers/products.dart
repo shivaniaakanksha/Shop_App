fc20fca20c80ca6501dd6e49e6f3d71b52ae7240
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-07-12 17:59:37 +0530

    showing-a-loading-indicator

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -67,9 +67,9 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  void addProduct(Product product) {
+  Future<void> addProduct(Product product) {
     const url = 'https://shop-app-6a23d.firebaseio.com/products.json';
-    http
+   return http
         .post(
       url,
       body: json.encode({
