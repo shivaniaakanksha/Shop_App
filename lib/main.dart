26d11eb87ed6244315ed3f2a9fe9e94d748fa9ca
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-07-14 00:03:31 +0530

    Automatically Logging Users In

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/splash-screen.dart';
 
 import './screens/cart_screen.dart';
+import './screens/splash-screen.dart';
 import './screens/products_overview_screen.dart';
 import './screens/product_detail_screen.dart';
 import './providers/products.dart';
@@ -18,7 +20,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider( 
+    return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(
           value: Auth(),
@@ -48,7 +50,15 @@ class MyApp extends StatelessWidget {
                 accentColor: Colors.deepOrange,
                 fontFamily: 'Lato',
               ),
-              home: auth.isAuth ? ProductsOverviewScreen() : AuthScreen(),
+              home: auth.isAuth
+                  ? ProductsOverviewScreen()
+                  : FutureBuilder(
+                      future: auth.tryAutoLogin(),
+                      builder: (ctx, authResultSnapshot) =>
+                          authResultSnapshot.connectionState ==
+                                  ConnectionState.waiting
+                              ? SplashScreen()
+                              : AuthScreen()),
               routes: {
                 ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
                 CartScreen.routeName: (ctx) => CartScreen(),
--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../models/http_exception.dart';
 
@@ -24,9 +25,11 @@ class Auth with ChangeNotifier {
     }
     return null;
   }
-  String get userId{
+
+  String get userId {
     return _userId;
   }
+
   Future<void> _authenticate(
       String email, String password, String urlSegment) async {
     final url =
@@ -55,8 +58,17 @@ class Auth with ChangeNotifier {
           ),
         ),
       );
-      _autoLogout(); 
+      _autoLogout();
       notifyListeners();
+      final prefs = await SharedPreferences.getInstance();
+      final userData = json.encode(
+        {
+          'token': _token,
+          'userId': _userId,
+          'expiryDate': _expiryDate.toIso8601String(),
+        },
+      );
+      prefs.setString('userData', userData);
     } catch (error) {
       throw error;
     }
@@ -70,22 +82,45 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, 'verifyPassword');
   }
 
-  void logout(){
-    _token =null;
-    _userId=null;
-    _expiryDate=null;
-     if(_authTimer != null){
+  Future<bool> tryAutoLogin() async {
+    final prefs = await SharedPreferences.getInstance();
+    if(!prefs.containsKey('userData')){
+      return false;
+    }
+    final extractedUserData = json.decode(prefs.getString('userData')) as Map<String, Object> ;
+    final expiryDate = DateTime.parse(extractedUserData['expiryDate']);
+
+    if(expiryDate.isBefore(DateTime.now())){
+      return false;
+    }
+
+    _token = extractedUserData['token'];
+    _userId = extractedUserData['userId'];
+    _expiryDate = expiryDate;
+    notifyListeners();
+    _autoLogout();
+    return true;
+  }
+
+  Future<void>  logout() async {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if (_authTimer != null) {
       _authTimer.cancel();
-      _authTimer=null;
+      _authTimer = null;
     }
     notifyListeners();
+    final prefs = await SharedPreferences.getInstance();
+    //prefs.remove('userData');
+    prefs.clear(); 
   }
 
   void _autoLogout() {
-    if(_authTimer != null){
+    if (_authTimer != null) {
       _authTimer.cancel();
     }
     final timeToExpiry = _expiryDate.difference(DateTime.now()).inSeconds;
-    _authTimer = Timer(Duration(seconds: timeToExpiry ),logout);
+    _authTimer = Timer(Duration(seconds: timeToExpiry), logout);
   }
 }
