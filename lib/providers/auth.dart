d84a20831916756831d0c7a16441cb9ea2fbab59
AAKANKSHA SHIVANI <shivaniaakanksha@gmail.com>
2019-07-13 12:02:29 +0530

    user-Login,error-handling while authentication

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -3,24 +3,42 @@ import 'dart:convert';
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 
+import '../models/http_exception.dart';
+
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/$urlSegment?key=AIzaSyDGvnMJuKwGmMLctCvF9f0vrlLJCy28mGw';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode(
+          {
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          },
+        ),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
   Future<void> signup(String email, String password) async {
-    const url =
-        'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDGvnMJuKwGmMLctCvF9f0vrlLJCy28mGw';
-    final response = await http.post(
-      url,
-      body: json.encode(
-        {
-          'email': email,
-          'password': password,
-          'returnSecureToken': true,
-        },
-      ),
-    );
-    print(json.decode(response.body));
+    return _authenticate(email, password, 'signupNewUser');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'verifyPassword');
   }
 }
